26f5f4eea7a8d83bae66c47732e389ea6848b525
Astrolic <ericantebo@live.se>
2021-09-08 15:53:52 +0000

    Add fields

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+default_row = ["⬛" for i in range(10)]
+fields = {k:default_row.copy() for k in list("ABCDEFGHIJ")}
+
+fields["A"][0] = "🔷"
+fields["J"][9] = "🔶"
+
+class Field:
+
+    def __init__(self):
+        pass
+
+selected = "⬜"
+friendly_capture = "🟦"
+enemy_capture = "🟧"
+
+fields["A"][1] = friendly_capture
+fields["J"][8] = enemy_capture
+
+def render_field(fields):
+
+    print("     0 1 2 3 4 5 6 7 8 9\n")
+
+    for colum_key in fields:
+        colum = f"{colum_key}   "
+        
+        for field in fields[colum_key]:
+            
+            colum = f"{colum}{field}" 
+        
+        print(colum)
+
+render_field(fields)
